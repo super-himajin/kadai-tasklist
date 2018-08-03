cbb3ab0ededbf433df6b2aee357a0b17cd1d6a59
himajin.super <himajindayo88@gmail.com>
2018-08-03 14:19:44 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'sessions/new'
 
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root to: 'tasks#index'
   resources :tasks
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
